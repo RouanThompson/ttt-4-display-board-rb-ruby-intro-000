b7a77bdbcf757041c48acc1494b80858a276ca42
rouan.thompson <rouan.thompson@gmail.com>
2020-02-17 02:59:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "#{board[0]}"
+  puts " #{board[0]} |"
+  puts " #{board[1]} |"
+  puts " #{board[2]}  "
+  puts "-----------"
+  puts " #{board[3]} |"
+  puts " #{board[4]} |"
+  puts " #{board[5]}  "
+  puts "-----------"
+  puts " #{board[6]} |"
+  puts " #{board[7]} |"
+  puts " #{board[8]}  "
 end
